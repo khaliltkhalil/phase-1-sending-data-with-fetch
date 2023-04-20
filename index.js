c6405f412721670a09d63bc1d051af25901302ba
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-04-20 10:07:03 -0500

    writes index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+  const data = {
+    name,
+    email,
+  };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const user = document.createElement("div");
+      user.textContent = data.id;
+      document.querySelector("body").appendChild(user);
+    })
+    .catch((error) => {
+      const errorMessage = document.createElement("div");
+      errorMessage.textContent = error.message;
+      document.querySelector("body").appendChild(errorMessage);
+      console.log(error.message);
+    });
+}
